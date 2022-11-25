98c026af01ea5e105998d94e6b1b82577d0cac74
ignaciomortega <89436962+ignaciomortega@users.noreply.github.com>
2022-11-25 08:12:47 -0300

    Valida formularios, añade funcionalidad ded reinicio

--- tareas/clase-6/tarea-clase6.js
+++ tareas/clase-6/tarea-clase6.js
@@ -2,14 +2,18 @@ const $botonCantidadFamiliares = document.querySelector("#boton-enviar-integrant
 const $formulario = document.querySelector("#form-edades");
 const $displayEstadisticas = document.querySelector("#estadisticas-finales");
 const $botonCalcularEstadisticas = document.querySelector("#boton-calcular-estadisticas");
+const $botonReinicio = document.querySelector("#reiniciar");
 
 $botonCantidadFamiliares.onclick = function(){
-    const cantidadFamiliares = document.querySelector("#cantidad-integrantes").value;
+    const cantidadFamiliares = Number(document.querySelector("#cantidad-integrantes").value);
+
+    if(cantidadFamiliares >= 1 && cantidadFamiliares <= 20 && Number.isInteger(cantidadFamiliares)) {
 
     for (let i = 0 ; i < cantidadFamiliares ; i++){
             let label = document.createElement("label");
             label.for = `edad-integrante${i+1}`;
             label.textContent = `Edad Integrante ${i+1}`;
+            label.className = "label-edad";
             $formulario.insertBefore(label, $botonCalcularEstadisticas);
             let input = document.createElement("input");
             input.type = "number";
@@ -21,6 +25,13 @@ $botonCantidadFamiliares.onclick = function(){
     }
     
     $botonCalcularEstadisticas.removeAttribute("hidden");
+    $botonCantidadFamiliares.setAttribute("disabled","true");
+    $botonReinicio.removeAttribute("hidden");
+    }
+
+    else {
+        alert("Solo podes ingresar una cantidad entre 1 y 20 integrantes!");
+    }
 
     return false
 }
@@ -33,9 +44,9 @@ function ordenarEdades(nodeListArray){
             edadesOrdenadas.push(nodeListArray[i].value);
     }
 
-    edadesOrdenadas.sort(function(a, b){return a - b})
+    edadesOrdenadas.sort(function(a, b){return a - b});
 
-    return edadesOrdenadas;
+    return edadesOrdenadas
 }
 
 function promediarEdades(arrayNumeros){
@@ -46,7 +57,7 @@ function promediarEdades(arrayNumeros){
 
     let promedio = total / arrayNumeros.length;
 
-    return promedio;
+    return promedio
 }
 
 
@@ -58,6 +69,21 @@ $botonCalcularEstadisticas.onclick = function(){
 
     $displayEstadisticas.textContent = `La mayor edad del grupo familiar es ${edades.at(-1)} años, la menor es de ${edades[0]}, y el promedio es de  ${promedio}`;
 
-    return false;
+    return false
 }
 
+$botonReinicio.onclick = function(){
+    const inputInicial = document.querySelector("#cantidad-integrantes");
+    inputInicial.value = "";
+
+    while($formulario.firstChild){
+        $formulario.firstChild.remove();
+    }
+
+    $formulario.innerHTML =`        <button type="submit" id="boton-calcular-estadisticas" hidden>Ver estadísticas</button>
+    <button typr="submit" id="reiniciar" hidden>Empezar de nuevo</button>`
+    $botonCantidadFamiliares.removeAttribute("disabled");
+    $displayEstadisticas.textContent ="";
+
+    return false
+}
